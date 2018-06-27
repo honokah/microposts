3a7a7acaa5cd6c6c4acea818a007b5db16aedd2e
honokaaa <ec2-user@ip-172-31-4-201.us-east-2.compute.internal>
2018-06-27 05:33:19 +0000

    kadai owatta

--- monolist/app/Http/Controllers/ItemsController.php
+++ monolist/app/Http/Controllers/ItemsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use \App\Item;
 
 class ItemsController extends Controller
 {
@@ -41,10 +42,11 @@ class ItemsController extends Controller
     {
       $item = Item::find($id);
       $want_users = $item->want_users;
+      $have_users = $item->have_users;
 
       return view('items.show', [
           'item' => $item,
-          'want_users' => $want_users,
+          'have_users' => $have_users,
       ]);
     }
     
